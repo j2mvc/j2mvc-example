baa6597bf74778f0c002beecbbf246deca492915
j2mvc <wwwdev@qq.com>
2019-07-08 17:34:00 +0800

    引入j2mvc-framework-web-2.1.15的包

--- j2mvc-example-web/docs/mysql.sql
+++ j2mvc-example-web/docs/mysql.sql
@@ -10,16 +10,17 @@ use j2mvc_example;
 -- 用户
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
-	id					varchar(32)	NOT NULL,
+	id					varchar(64)	NOT NULL,
 	username			varchar(255)	NOT NULL COMMENT '用户名',			
-	password			varchar(255)	NOT NULL COMMENT '密码',				
+	password			varchar(255)	NOT NULL COMMENT '密码',		
+	token				varchar(255)	NOT NULL COMMENT '密码',	
   	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 购物车
 DROP TABLE IF EXISTS products;
 CREATE TABLE IF NOT EXISTS products(
-	id					varchar(32)		NOT NULL,
+	id					varchar(64)		NOT NULL,
 	title				varchar(255)	NOT NULL COMMENT '商品标题',			
 	detail				varchar(255)			 COMMENT '商品详情',				
 	price				double(11,2)	NOT NULL COMMENT '价格',		
@@ -34,7 +35,7 @@ insert products values
 -- 购物车
 DROP TABLE IF EXISTS carts;
 CREATE TABLE IF NOT EXISTS carts(
-	id					varchar(32)	NOT NULL,
+	id					varchar(64)	NOT NULL,
 	user_id				varchar(255)	NOT NULL COMMENT '用户ID',			
 	product_id			varchar(255)	NOT NULL COMMENT '商品ID',				
 	num					int(11)	NOT NULL COMMENT '数量',				
